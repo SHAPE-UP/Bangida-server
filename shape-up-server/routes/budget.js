65d31735639df5a140a5160e016312577d225d46
hwangsungmin <hwangsungmin_00@naver.com>
2022-07-24 11:53:18 +0900

    budget 라우터 작성(삽입, 삭제)

--- shape-up-server/routes/budget.js
+++ shape-up-server/routes/budget.js
@@ -4,9 +4,42 @@ const { Budget } = require('../models/Budget');
 
 
 // 전체 예산서 불러오기: post
+router.post("/getBudget", (req, res) => {
+    Budget.find({ familyID: req.body.family })
+    .exec((err, budget) => {
+        if (err) return res.status(400).send(err);
+        return res.status(200).send({ success: true, budget });
+    });
+});
+
 
 // 예산서 등록: post
+router.post("/registerBudget", (req, res) => {
+    let newbudget = new Budget(req.body)
+    newbudget.save(
+        (err, budgetInfo) => {
+            if(err) return res.status(400).json({ success: false, err })
+            return res.status(200).json({
+                success: true,
+                budgetInfo
+            })
+        }
+    )
+});
 
 // 예산서 삭제: delete
+
+router.delete("/deleteBudget", (req,res)=>{
+    Budget.deleteOne(req.body)
+    .exec( 
+        (err, budgetInfo) => {
+            if(err) return res.status(400).json({ success: false, err })
+            return res.status(200).json({
+                success: true,
+                budgetInfo
+            })
+        }
+    )
+});
 
 module.exports = router;
