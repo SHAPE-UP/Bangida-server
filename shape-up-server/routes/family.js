4a4aaf31514e58f0acf4de14ca71ce4ad9752650
hwangsungmin <hwangsungmin_00@naver.com>
2022-08-13 14:52:41 +0900

    반려견 입양시 PetId 생성 push

--- shape-up-server/routes/family.js
+++ shape-up-server/routes/family.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { Family } = require('../models/Family');
+const { Pet } = require('../models/Pet');
 
 
 // 가족 구성원 정보 불러오기: post
@@ -12,6 +13,23 @@ router.post("/getFamily", (req, res) => {
         return res.status(200).json({ success: true, family });
       });
 });
+
+// 반려견 입양시 PetID push
+router.put("/additionPetId", (req,res) => { 
+  const petn = new Pet()
+  
+  petn.save((err, petInfo) => {
+    Family.findOneAndUpdate({ _id: req.body._id }, {pet: petInfo.id})
+    .exec(
+      (err, todoInfo) => {
+        if(err) return res.status(400).json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+      }
+    )
+  })  
+})
 
 
 
